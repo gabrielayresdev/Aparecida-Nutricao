ef8c8dba824d9369e73949b657f1e0a70434aa22
Gabriel Ayres <44467957+INeonysI@users.noreply.github.com>
2022-09-01 20:10:14 -0300

    Atualização das funções de validação

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -5,16 +5,33 @@ for (let i = 0; i < paciente.length; i++) {
     let altura = paciente[i].querySelector(".info-altura").textContent
     let imc = paciente[i].querySelector(".info-imc")
 
-    if (peso < 0 || peso > 1000) {
+    if (!validaPeso(peso)) {
         console.log("Peso inválido")
         imc.textContent = "Peso inválido"
         paciente[i].classList.add("paciente-invalido")
-    } else if (altura < 0 || altura > 4) {
+    } else if (!validaAltura(altura)) {
         console.log("Altura inválida")
         imc.textContent = "Altura inválida"
         paciente[i].classList.add("paciente-invalido")
     } else {
         imc.textContent = calcImc(peso, altura)
+    }
+}
+
+function validaPeso(peso) {
+    if ((peso > 0 && peso < 1000) || peso == "") {
+        return true
+    } else {
+        console.log(peso + "é o peso")
+        return false
+    }
+}
+
+function validaAltura(altura) {
+    if ((altura > 0 && altura < 3) || altura == "") {
+        return true
+    } else {
+        return false
     }
 }
 
