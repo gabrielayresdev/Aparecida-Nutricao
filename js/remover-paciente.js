481f8d4f8f05f079b0ca8094714bde5131d9560c
Gabriel Ayres <44467957+INeonysI@users.noreply.github.com>
2022-09-02 13:34:46 -0300

    Adicionado tempo para remoção da tabela

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,6 +1,9 @@
 let tabela = document.querySelector("#tabela-pacientes")
 
 tabela.addEventListener("dblclick", function (event) {
-    event.target.parentNode.remove()
+    event.target.parentNode.classList.add("fadeOut")
+    setTimeout(function () {
+        event.target.parentNode.remove()
+    }, 500)
 })
 
